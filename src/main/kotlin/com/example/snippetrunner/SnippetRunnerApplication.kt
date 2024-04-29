250fe5db8e9e0761c25e4464578bbb6948fb37e8
franciscocavallaro <ccavallaro00@gmail.com>
2024-04-29 20:23:02 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/snippetrunner/SnippetRunnerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.snippetrunner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetRunnerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetRunnerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/snippetrunner/SnippetRunnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.snippetrunner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetRunnerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
